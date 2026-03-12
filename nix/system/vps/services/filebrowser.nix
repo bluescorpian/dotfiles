89f6ad9422a53449378e7ee079c5e49017c77d85
Harry Kruger <harry@hrry.sh>
2026-03-12 23:29:14 +0200

    Refactor FileBrowser service configuration to use systemd and enhance tmpfiles rules

--- nix/system/vps/services/filebrowser.nix
+++ nix/system/vps/services/filebrowser.nix
@@ -1,16 +1,23 @@
-{ domain, ... }:
+{ domain, pkgs, ... }:
 let
   port = 3006;
   subdomain = "files.${domain}";
 in {
-  services.filebrowser = {
-    enable = true;
-    settings = {
-      port = port;
-      root = "/srv/smb";
-      address = "127.0.0.1";
+  systemd.services.filebrowser = {
+    description = "FileBrowser";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.filebrowser}/bin/filebrowser -a 127.0.0.1 -p ${toString port} -r /srv/smb -d /srv/filebrowser/database.db";
+      Restart = "always";
+      User = "harry";
     };
   };
+
+  systemd.tmpfiles.rules = [
+    "d /srv/filebrowser 0700 harry users -"
+  ];
 
   services.caddy.virtualHosts.${subdomain}.extraConfig = ''
     reverse_proxy localhost:${toString port}
